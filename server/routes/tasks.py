6b608691ebf426252eefc3201a9df04154bd8b57
mathew holleron <mattyholleron@gmail.com>
2023-03-24 16:03:02 +0000

    task routes

---
+++ server/routes/tasks.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from models.index import Project, Task
+
+
+
+@app.route('/projects/<int:project_id>/tasks', methods=['POST'])
+def create_task(project_id):
+    project = Project.query.get_or_404(project_id)
+    data = request.get_json()
+    task = Task(description=data['description'], time_estimate=data['time_estimate'], project_id=project_id)
+    db.session.add(task)
+    db.session.commit()
+    return jsonify(task.serialize()), 201
+
+@app.route('/projects/<int:project_id>/tasks/<int:task_id>', methods=['PUT'])
+def update_task(project_id, task_id):
+    project = Project.query.get_or_404(project_id)
+    task = Task.query.filter_by(id=task_id, project_id=project_id).first_or_404()
+    data = request.get_json()
+    if 'description' in data:
+        task.description = data['description']
+    if 'time_estimate' in data:
+        task.time_estimate = data['time_estimate']
+    if 'completed' in data:
+        task.completed = data['completed']
+    if 'time_actual' in data:
+        task.time_actual = data['time_actual']
+    db.session.commit()
+    return jsonify(task.serialize()), 200
+
+@app.route('/projects/<int:project_id>/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(project_id, task_id):
+    project = Project.query.get_or_404(project_id)
+    task = Task.query.filter_by(id=task_id, project_id=project_id).first_or_404()
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({}), 204
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
